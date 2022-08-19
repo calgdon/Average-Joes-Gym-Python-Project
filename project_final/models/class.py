097a809d3ecd3084c38b53314aa10b29ddd9ca8f
Callum Donaldson <callumdonaldson1990@gmail.com>
2022-08-19 15:07:00 +0100

    class setup

---
+++ project_final/models/class.py
@@ -0,0 +1,10 @@
+class Class:
+
+    def __init__(self, name, time, date, location, instructor, capacity, id):
+        self.name = name
+        self.time = time
+        self.date = date
+        self.location = location
+        self.instructor = instructor
+        self.capacity = capacity
+        self.id = id
---
+++ project_final/models/members.py
@@ -0,0 +1,11 @@
+class Members:
+
+    def __init__(self, first_name, last_name, date_of_birth, address, tel_number, email, membership_type, id):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.date_of_birth = date_of_birth
+        self.address = address
+        self.tel_number = tel_number
+        self.email = email
+        self.membership_type = membership_type
+        self.id = id
