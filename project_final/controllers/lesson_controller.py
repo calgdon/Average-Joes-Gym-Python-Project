72c3ab1c665d6f669806f998034ad2b355fe9403
Callum Donaldson <callumdonaldson1990@gmail.com>
2022-08-22 15:16:04 +0100

    lessons all completed

--- project_final/controllers/lesson_controller.py
+++ project_final/controllers/lesson_controller.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, Flask, redirect, render_template, request
+from models.lesson import Lesson
 
 import repositories.visit_repository as visit_repository
 import repositories.lesson_repository as lesson_repository
@@ -37,10 +38,51 @@ def edit_lesson(id):
 
 # Update the lesson
 
+@lessons_blueprint.route("/lessons/<id>/edit", methods=["POST"])
+def post_edit_lesson(id):
+        name = request.form["name"]
+        time = request.form["time"]
+        date = request.form["date"]
+        location_id = request.form["location_id"]
+        instructor_id = request.form["instructor_id"]
+        capacity = request.form["capacity"]
+        id = id
+        location = location_repository.select(location_id)
+        instructor = instructor_repository.select(instructor_id)
+        updated_lesson = Lesson(name, time, date, location, instructor, capacity, id)
+        lesson_repository.update(updated_lesson)
+        return redirect("/lessons")
+
 
 # Delete a single lesson
 
 @lessons_blueprint.route("/lessons/<id>/delete", methods=["GET"])
 def delete_lesson(id):
     lesson_repository.delete(id)
+    return redirect("/lessons")
+
+
+# Add a new lesson
+
+@lessons_blueprint.route("/lessons/new")
+def add_lesson():
+    locations = location_repository.select_all()
+    instructors = instructor_repository.select_all()
+    return render_template("/lessons/add_lesson.html", locations=locations, instructors=instructors)
+
+
+# Post the new lesson
+
+@lessons_blueprint.route("/lessons/add", methods=["POST"])
+def post_new_lesson():
+    name = request.form["name"]
+    time = request.form["time"]
+    date = request.form["date"]
+    location_id = request.form["location_id"]
+    instructor_id = request.form["instructor_id"]
+    capacity = request.form["capacity"]
+    location = location_repository.select(location_id)
+    instructor = instructor_repository.select(instructor_id)
+    new_lesson = Lesson(name, time, date, location, instructor, capacity)
+    lesson_repository.save(new_lesson)
     return redirect("/lessons")
